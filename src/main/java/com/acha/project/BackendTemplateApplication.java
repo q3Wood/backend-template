969e6062fd1089562b6ef3d51525a8926ed7a6f8
q3Wood <scau.czr@gmail.com>
2026-02-01 18:11:01 +0800

    init: 初始化 Spring Boot 3 脚手架

---
+++ src/main/java/com/acha/project/BackendTemplateApplication.java
@@ -0,0 +1,14 @@
+package com.acha.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendTemplateApplication.class, args);
+	}
+
+}
+// group atrifact Package name
---
+++ src/test/java/com/acha/project/BackendTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acha.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
